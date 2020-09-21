5e6c7003fd71679ae383497217361732bbb53730
devteamworkz <blizzdata1@gmail.com>
2020-09-21 23:43:47 +0300

    contacts.reducers contactUpdatedSuccess handler

--- src/app/modules/main/store/reducers/contacts.reducers.ts
+++ src/app/modules/main/store/reducers/contacts.reducers.ts
@@ -1,13 +1,22 @@
 import { createReducer, on } from '@ngrx/store';
 import { Contact } from '../../models/contact.model';
-import { contactsLoadedSuccess, loadContacts } from '../actions/contacts.actions';
+import { contactsLoadedSuccess, contactUpdatedSuccess, loadContacts } from '../actions/contacts.actions';
 
-let initialState: Contact[];
+let initialState: Contact[] = [];
 
 const _contactsReducers = createReducer(
   initialState,
   on(loadContacts, () => undefined),
-  on(contactsLoadedSuccess, (_, { payload }) => payload)
+  on(contactsLoadedSuccess, (_, { payload }) => payload),
+  on(contactUpdatedSuccess, (state, { payload }) => {
+    state = [...state];
+
+    const index = state.findIndex((contact) => contact.id === payload.id);
+
+    state[index] = payload;
+
+    return state;
+  })
 );
 
 export function contactsReducers(state, action) {
