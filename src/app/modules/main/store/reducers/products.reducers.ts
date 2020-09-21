4764bd53eb3c00be1c92eb9a5928c149ab9f91e9
devteamworkz <blizzdata1@gmail.com>
2020-09-21 23:43:27 +0300

    products.reducers productUpdatedSuccess handler

--- src/app/modules/main/store/reducers/products.reducers.ts
+++ src/app/modules/main/store/reducers/products.reducers.ts
@@ -1,13 +1,22 @@
 import { createReducer, on } from '@ngrx/store';
 import { Product } from '../../models/product.model';
-import { loadProducts, productsLoadedSuccess } from '../actions/products.actions';
+import { loadProducts, productsLoadedSuccess, productUpdatedSuccess } from '../actions/products.actions';
 
 let initialState: Product[] = [];
 
 const _productsReducers = createReducer(
   initialState,
   on(loadProducts, () => undefined),
-  on(productsLoadedSuccess, (_, { payload }) => payload)
+  on(productsLoadedSuccess, (_, { payload }) => payload),
+  on(productUpdatedSuccess, (state, { payload }) => {
+    state = [...state];
+
+    const index = state.findIndex((product) => product.id === payload.id);
+
+    state[index] = payload;
+
+    return state;
+  })
 );
 
 export function productsReducers(state, action) {
