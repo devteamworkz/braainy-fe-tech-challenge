83cee111b0c90c406ed77cceead6cd612dd44f7e
devteamworkz <blizzdata1@gmail.com>
2020-09-21 22:39:02 +0300

    added ngrx-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule.forRoot()],
+  imports: [BrowserModule, AppRoutingModule, SharedModule.forRoot(), StoreModule.forRoot({}), EffectsModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
